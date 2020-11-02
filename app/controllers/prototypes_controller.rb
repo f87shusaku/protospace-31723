9bb52c4108ed8d88d0270ac9d39f5647087474f0
f87shusaku <monkye0114@gmil.com>
2020-11-02 16:57:28 +0900

    変更

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,6 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, only: [:new]
+
   def index
     @prototypes = Prototype.all
   end
